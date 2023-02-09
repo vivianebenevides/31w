abf17aef32ac6435d04f1501a14664a6fb1e88fa
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-02-09 15:22:08 -0500

    c4 register_menu, add_theme_support, creation d'un pour le titre pricipal du site

--- functions.php
+++ functions.php
@@ -8,3 +8,27 @@ function ajouter_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
 
+/* ----------------------------------- Add_theme_support */
+
+add_theme_support( 'html5', 
+                    array( 'search-form', 
+                           'comment-form', 
+                           'comment-list', 
+                           'gallery', 
+                           'caption' ) );
+
+add_theme_support( 'title-tag' );
+
+add_theme_support( 'custom-logo', 
+                    array(
+                        'height' => 150,
+                        'width'  => 150,
+                            ) );
+/*---------------------------------- Enregistrement des menus */
+    function enregistrement_des_menus(){
+        register_nav_menus( array(
+            'menu_entete' => 'Menu entête',
+            'menu_footer'  => 'Menu pied de page',
+        ) );
+    }
+    add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
