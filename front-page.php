100e6fc3f926d03d76bdf93b53e22916ddc0b101
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-02-16 16:46:14 -0500

    c6 importer md et modifier page d'accueil

--- front-page.php
+++ front-page.php
@@ -6,19 +6,18 @@ get_header() ?>
 <main>
     <code>front-page.php</code>
     <h3>index.php</h3>
-<?php 
-if (have_posts()):
-    while (have_posts()) : the_post();
-        // the_title('<h1>','</h1>');
-        // the_permalink(); ?>
-        <h1><a href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></h1>
-        
-        <?php echo wp_trim_words(get_the_excerpt(), 4);
-        // the_content();
-        // the_excerpt();
-    endwhile;
-endif;
-?>   
+    <section class="blocflex">
+        <?php 
+        if (have_posts()):
+            while (have_posts()) : the_post();
+                // the_title('<h1>','</h1>');
+                // the_permalink(); ?>
+                <article>
+                    <h3><a href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></h3>
+                    <p><?php echo wp_trim_words(get_the_excerpt(), 4);?></p>
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>  
+    </section>
 </main> 
 <?php get_footer(); ?>
-
