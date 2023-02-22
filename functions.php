a48bfd128e972d7ddd90d79cbbfc0b14fee9eac4
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-02-22 12:14:31 -0500

    c7 insertion du fichier cours.sql et note des cours(markdown) et ajoute de la function modifie requete principal (fichier function.php)

--- functions.php
+++ functions.php
@@ -23,10 +23,29 @@ add_theme_support( 'custom-logo',
                         'width'  => 150,
                             ) );
 /*---------------------------------- Enregistrement des menus */
-    function enregistrement_des_menus(){
-        register_nav_menus( array(
-            'menu_entete' => 'Menu entête',
-            'menu_footer'  => 'Menu pied de page',
-        ) );
+function enregistrement_des_menus(){
+    register_nav_menus( array(
+        'menu_entete' => 'Menu entête',
+        'menu_footer'  => 'Menu pied de page',
+    ) );
+}
+add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
+
+
+/**
+ * Modifie la requete principale de Wordpress avant qu'elle soit exécuté
+ * le hook « pre_get_posts » se manifeste juste avant d'exécuter la requête principal
+ * Dépendant de la condition initiale on peut filtrer un type particulier de requête
+ * Dans ce cas çi nous filtrons la requête de la page d'accueil
+ * @param WP_query  $query la requête principal de WP
+ */
+function cidweb_modifie_requete_principal( $query ) {
+    if (    $query->is_home() //si page d'accueil
+            && $query->is_main_query()  //si requête principale
+            && ! is_admin() ) { // non tableau de bord
+        $query->set( 'category_name', 'note-wp' ); // filtre les articles de categorie "note-wp"
+        $query->set( 'orderby', 'title' ); //trie selon se titre
+        $query->set( 'order', 'ASC' ); // en ordre ascendent 
+        }
     }
-    add_action( 'after_setup_theme', 'enregistrement_des_menus', 0 );
+add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
