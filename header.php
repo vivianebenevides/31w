b20f35dee8effb30109082eb3d1fff94b0ba174b
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-03-08 10:02:18 -0500

    c11 aside est retire de la page d'accueil

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <title>Document</title>
     <?php wp_head(); ?>
 </head>
-<body class="site">
+<body class="site <?= (is_front_page() ? "no-aside" : ""); ?> ">
     <header class="site__entete">
         <section class="logomenu">
             <?php the_custom_logo(); ?>
@@ -22,4 +22,9 @@
         <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
         <h2><?php bloginfo('description'); ?></h2> 
     </header>
-    <?php get_template_part("template-parts/aside"); ?>
+    <?php
+    if(!is_front_page())
+    {
+     get_template_part("template-parts/aside");
+    }
+     ?>
