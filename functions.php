5d81272e409d98e5eb0f7b2662c54d7701b1a33f
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-03-08 09:39:36 -0500

    c11 modification des titres du menu cours

--- functions.php
+++ functions.php
@@ -49,3 +49,19 @@ function cidweb_modifie_requete_principal( $query ) {
         }
     }
 add_action( 'pre_get_posts', 'cidweb_modifie_requete_principal' );
+
+/**
+ * Permet de modifier les titres tu menu "cours"
+ * @param $title : titre du choix menu
+ * @param $item : le choix global
+ * @param $args : Object qui représent la structure de menu
+ */
+function perso_menu_item_title($title, $item, $args) {
+    // Remplacer 'nom_de_votre_menu' par l'identifiant de votre menu
+    if($args->menu == 'cours') {
+// Modifier la longueur du titre en fonction de vos besoins
+$title = wp_trim_words($title, 3, ' ... ');
+}
+return $title;
+}
+add_filter('nav_menu_item_title', 'perso_menu_item_title', 10, 3);
