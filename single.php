debd6d61924e004b88b30819d439c92a4a2286a7
Viviane Silva Holanda Freitas Benevides <vivianebenevides@air-de-viviane.home>
2023-04-04 22:12:22 -0400

    c18 ajustement des tailles de police pour une meilleure adaptation dans le responsive et optimisation du pied de page responsive

--- single.php
+++ single.php
@@ -9,7 +9,7 @@ get_header() ?>
     if (have_posts()):
         while (have_posts()) : the_post();
             // usar funcao in_category (ver aside em template-parts)
-            the_title('<h1>','</h1>');
+            the_title('<h2>','</h2>');
             the_content();
         endwhile;
     endif;
