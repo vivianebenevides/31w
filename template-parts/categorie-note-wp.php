f4a5b6813c302da8ea1832144e85604684ee0d38
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-04-05 13:27:56 -0400

    c19 ajout des champs personnalisés dans les articles de note de cours

--- template-parts/categorie-note-wp.php
+++ template-parts/categorie-note-wp.php
@@ -10,5 +10,7 @@ $titre = get_the_title();
 <article class="blocflex__article">
     <h5><a href="<?php the_permalink(); ?>"> <?= $titre; ?></a></h5>
     <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+    <p><?php the_field('temps_lecture'); ?> </p>
+    <p>Niveu de difficulté : <?php the_field('niveau_difficulte'); ?> </p>
 </article>
 
