bf7040be1cc0487405568986cb075b53866bfa02
Viviane Silva Holanda Freitas Benevides <vivianebenevides@air-de-viviane.home>
2023-04-05 00:03:23 -0400

    c18 creation du fichier page_404.scss pour ajouter des styles a la page 404, amélioration de la conception de la page 404 et ajout de style

--- 404.php
+++ 404.php
@@ -3,9 +3,18 @@
     Modèle index.php représente le modèle par défaut du thème
 */
 get_header() ?>
-<main class="site__main">
-    <code>404.php</code>
-    <h1>Erreur 404 - la page n'existe pas</h1>
+<!-- <main class="site__main"> -->
+<main>
+    <!-- <code>404.php</code> -->
+    <section class="page-404">
+        <h1>Erreur</h1>
+        <div class="nb-404">
+            <p>4</p>
+            <img src="https://s2.svgbox.net/hero-outline.svg?ic=exclamation-circle&color=ae3f26" width="200" height="200"> 
+            <p>4</p>
+        </div>
+        <h2>Atention, cette page n'existe pas...</h2>
+    </section>
 </main> 
-<?php get_footer(); ?>
+<?php get_footer(); ?>  
 
