ad467a2f0885882d0ae12d7ba7d3b62529b93607
Viviane Silva Holanda Freitas Benevides <vivianebenevides@MacBook-Air-de-Viviane.local>
2023-04-18 16:24:54 -0400

    ef-étape 4 ajout des menus cours et notes de cours

--- 404.php
+++ 404.php
@@ -11,8 +11,15 @@ get_header() ?>
         <h2>Page introuvable, vous pouvez tenter une recherche</h2>
 
         <div class="recherche-404">
-                    <?= get_search_form(); ?>
+            <?= get_search_form(); ?>
         </div>
+        
+        <h3>Nous choix de cours</h3>
+        <?php wp_nav_menu( array( 'menu' => 'menu404', 'menu_class' => 'menu404' ) ); ?>
+
+        <h3>Les notes de cours</h3>
+        <?php wp_nav_menu( array( 'menu' => 'menu404-notes', 'menu_class' => 'menu404' ) ); ?>
+
     </section>
 </main> 
 <?php get_footer(); ?>  
